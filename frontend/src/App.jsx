7139b7afa1666986f48ba984c0f144acceb8463b
yena1717 <yanwe294@gmail.com>
2026-09-19 23:00:48 +0900

    Add routes for SignUp and MainPage components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import MainPage from './pages/MainPage'
+import SignUp from "./pages/SignUp";
 import CreateSpace from './pages/CreateSpace'
 import JoinSpace from './pages/JoinSpace'
 import SpacePage from './pages/SpacePage'
@@ -9,6 +11,8 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/main" element={<MainPage />} />
       <Route path="/create" element={<CreateSpace />} />
       <Route path="/join" element={<JoinSpace />} />
       <Route path="/spaces/:spaceId" element={<SpacePage />} />
